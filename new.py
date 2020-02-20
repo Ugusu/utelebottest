45809e9ff9d8f803775a22957308bfcdf366bc17
Ughur <ugur.mamedzade@gmail.com>
2020-02-20 22:16:13 +0400

    first time

---
+++ new.py
@@ -0,0 +1,10 @@
+import telebot
+
+bot = telebot.TeleBot("931938061:AAGiYIbGRmIW-wBbfVgxoeTHhXIxwQe2E-o")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	#bot.reply_to(message, message.text)
+    bot.send_message(message.chat.id, message.text)
+    
+bot.polling(none_stop = True)
